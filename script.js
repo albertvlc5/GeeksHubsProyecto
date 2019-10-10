db40b35b4462393a9bad9a50a589027633f95568
Albert Pla <alplsan@epsa.upv.es>
2019-10-10 16:11:58 +0200

    primera version

---
+++ script.js
@@ -0,0 +1,54 @@
+SC.initialize({
+    client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb'
+});
+
+document.querySelector('.buscarCancion').addEventListener('submit', function(event) {
+    event.preventDefault();
+    console.log(event.target.busqueda.value);
+    SC.get('/tracks', {
+            q: event.target.busqueda.value
+        })
+        .then(function(res) {
+            document.querySelector('.results').innerHTML = '';
+            console.log(res)
+            var cincocaratulas = res.slice(0, 5);
+            for (let i = 0; i < cincocaratulas.length; i++) {
+                const imagen = document.createElement('img')
+                imagen.src = res[i].artwork_url == null ? imagen.src = "./nodisponible.jpg" : res[i].artwork_url
+                imagen.id = res[i].id
+                imagen.draggable = "true";
+                imagen.ondragstart = function(ev) {
+                    drag(ev)
+                };
+                imagen.classList.add('editor');
+                document.querySelector('.results').append(imagen);
+            }
+        })
+})
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+
+function drag(ev) {
+    ev.dataTransfer.setData("identificador", ev.target.id);
+    ev.dataTransfer.setData("srcCancion", ev.target.src);
+}
+
+
+function drop(ev) {
+    ev.preventDefault();
+
+    var identificador = ev.dataTransfer.getData("identificador");
+    var srcCancion = ev.dataTransfer.getData("srcCancion");
+
+    ev.target.src = srcCancion;
+
+
+    SC.stream('/tracks/' + identificador).then(function(player) {
+        player.play();
+    }).catch(function(error) {
+        alert('Error : ' + error.message);
+    });
+}
