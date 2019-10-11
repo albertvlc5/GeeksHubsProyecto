5330cc9231b0c6ff12c7949c2bddeea96dc8da52
Albert Pla <alplsan@epsa.upv.es>
2019-10-11 13:25:32 +0200

    Boton Play/Pause OK

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var cancion = null;
 SC.initialize({
     client_id: 'aa06b0630e34d6055f9c6f8beb8e02eb'
 });
@@ -47,34 +48,19 @@ function drop(ev) {
 
     SC.stream('/tracks/' + identificador).then(function(player) {
         player.play();
+        cancion = player;
+
     }).catch(function(error) {
         alert('Error : ' + error.message);
     });
 
 }
 
-// var button = document.getElementById("button");
-// var identificador = document.getElementById("identificador");
-
-// button.addEventListener("click", function() {
-//     if (identificador.paused) {
-//         identificador.play();
-//         button.innerHTML = "Pause";
-//     } else {
-//         audio.pause();
-//         button.innerHTML = "Play";
-//     }
-// });
-
 function playPause(ev) {
-    var identificador = ev.dataTransfer.getData("identificador");
-    SC.stream('/tracks/' + identificador).then(function(player) {
-        player.pause();
-    });
+    console.log(cancion, ev)
+    if (!cancion.isPlaying()) {
+        cancion.play();
+    } else {
+        cancion.pause();
+    }
 }
-// function playPause(ev) {
-//     var identificador = ev.dataTransfer.getData("identificador");
-
-//     SC.stream('/tracks/' + identificador).then(function(player) {
-//         player.pause();
-//     })};
